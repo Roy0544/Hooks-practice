3ce1d089f9ad78b8c209b206692fd6cbaff66ee9
roy0544 <roycomp44@gmail.com.com>
2025-08-13 20:10:55 +0530

    adding my project to my github

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Timer from './Components/Timer'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>hello</h1>
+     <Timer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Timer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Timer() {
+  return (
+    <div>
+        <h1 className='text-black'>lets chekc it </h1>
+    </div>
+  )
+}
+
+export default Timer
